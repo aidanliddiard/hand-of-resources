14bc005cebf613960e0027d47281c04da4150473
aidanliddiard <aidanliddiard@gmail.com>
2022-06-16 10:07:57 -0700

    flowers table set up

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,16 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+DROP TABLE IF EXISTS flowers;
+
+CREATE TABLE flowers (
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  name VARCHAR NOT NULL,
+  origin VARCHAR 
+);
+
+INSERT INTO flowers (
+  name,
+  origin
+)
+
+VALUES ('Calla lily', 'South Africa'), ('Iris', 'Virginia'), ('Hydrangea', 'Japan')
