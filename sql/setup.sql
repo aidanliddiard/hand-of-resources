4f7a5bb670bb2323e69cdf5b8fc6c4373460939f
aidanliddiard <aidanliddiard@gmail.com>
2022-06-17 08:26:31 -0700

    fruit table set up

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,6 +1,7 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
 DROP TABLE IF EXISTS flowers;
+DROP TABLE IF EXISTS fruit;
 
 CREATE TABLE flowers (
   id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
@@ -8,9 +9,25 @@ CREATE TABLE flowers (
   origin VARCHAR 
 );
 
+CREATE TABLE fruit (
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  name VARCHAR NOT NULL,
+  fav_pairing VARCHAR,
+  origin VARCHAR
+);
+
 INSERT INTO flowers (
   name,
   origin
 )
 
-VALUES ('Calla lily', 'South Africa'), ('Iris', 'Virginia'), ('Hydrangea', 'Japan')
+VALUES ('Calla lily', 'South Africa'), ('Iris', 'Virginia'), ('Hydrangea', 'Japan');
+
+INSERT INTO fruit (
+  name, 
+  fav_pairing,
+  origin
+)
+
+VALUES ('Raspberry', 'Anything chocolate', NULL), ('Apple', 'Cheese', 'Kazakhstan'), ('Grapes', NULL, 'Asia, Europe, North America')
+
